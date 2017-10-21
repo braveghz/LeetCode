ac4d089f1ac5f9636118e37521e1139c42aa5578
braveghz <braveghz@gmail.com>
2017-10-21 23:10:04 +0800

    116 ms

---
+++ 1/main.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        size = len(nums)
+        re_num = {}
+        for i in range(size):
+            tofind = target - nums[i]
+            if tofind in re_num:
+                return [re_num[tofind], i]
+            re_num[nums[i]] = i
+
+
+def main():
+    s = Solution()
+    print s.twoSum([2, 7, 11, 15], 9)
+    print s.twoSum([3, 2, 4], 6)
+    print s.twoSum([2, 5, 5, 11], 10)
+
+
+if __name__ == '__main__':
+    main()
